584f5c8595e8540196168bef38692b808ec87e24
Maik Glatki <maik.glatki@gmail.com>
2014-06-27 12:31:04 +0200

    Fixed debian kernel remover: Do not remove linux-image-virtual.

--- debian_remove_unused_kernels.sh
+++ debian_remove_unused_kernels.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # remove unused kernels in debian. Included newer kernels than the running one.
 
-apt-get remove $(dpkg -l| egrep '^ii  linux-(im|he)'|awk '{print $2}'| grep -v $(uname -r))
+apt-get remove $(dpkg -l| egrep '^ii  linux-(im|he)'| grep -v linux-image-virtual | awk '{print $2}'| grep -v $(uname -r))
+
+echo "Done. Now run aptitude upgrade to fix broken stuff, please."
