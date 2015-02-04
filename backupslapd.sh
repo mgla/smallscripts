4ca03953c5717ab1630c4c2cc0ba6dbec225b713
root <root@qwiki.(none)>
2015-02-04 16:05:33 +0100

    Refine slapd backup script a bit.

--- backupslapd.sh
+++ backupslapd.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Ugly script to backup a slapd ldap database.
+set -u
 
 FILE=ldap.diff
 if [ ! -e $FILE ]; then
 	echo "creating backup as $FILE"
-	slapcat -v -l ldap.diff
+	slapcat -o ldif-wrap=no -l ldap.diff
 	exit 0
 else
 	echo "$FILE exists, aborting."
