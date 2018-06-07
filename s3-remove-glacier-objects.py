de7b1962b054aa2600736d27224facedfb632509
Maik Glatki <maik@glatki.eu>
2018-06-07 13:32:17 +0000

    Small changes to satisfy pylint.

--- s3-remove-glacier-objects.py
+++ s3-remove-glacier-objects.py
@@ -2,58 +2,66 @@
 # Removes s3 glacier objects from an s3 bucket
 # Author: Maik Glatki <maik@glatki.eu>, 2018
 
-from botocore import credentials
-import botocore.session
-import boto3
 import os
 import sys
 
-# Boto3 MFA session magic
-working_dir = os.path.join(os.path.expanduser('~'),'.aws/cli/cache')
-
-session = botocore.session.get_session()
-provider = session.get_component('credential_provider').get_provider('assume-role')
-provider.cache = credentials.JSONFileCache(working_dir)
+from botocore import credentials
+import botocore.session
+import boto3
 
 # Set Variables
-s3_bucket="example"
-s3_prefix="exampleprefix"
+S3_BUCKET = "example"
+S3_PREFIX = "exampleprefix"
 
-client = boto3.Session(botocore_session=session).client('s3')
+def get_session():
+    """# Boto3 MFA session magic"""
+    working_dir = os.path.join(os.path.expanduser('~'), '.aws/cli/cache')
 
-objects_left = True
-next_token = ''
-iterated = 0
-deleted = 0
-while objects_left:
-    if next_token is '':
-        res = client.list_objects_v2(
-            Bucket=s3_bucket,
-            MaxKeys=1000,
-            Prefix=s3_prefix
-        )
-    else:
-        res = client.list_objects_v2(
-            Bucket=s3_bucket,
-            MaxKeys=1000,
-            Prefix=s3_prefix
-            ContinuationToken=next_token
-        )
+    session = botocore.session.get_session()
+    provider = session.get_component('credential_provider').get_provider('assume-role')
+    provider.cache = credentials.JSONFileCache(working_dir)
+    return session
 
-    glacier_keys = [{'Key': obj['Key']}  for obj in res['Contents'] if obj['StorageClass'] == 'GLACIER']
-    iterated += len(res['Contents'])
-    deleted += len(glacier_keys)
-    if len(glacier_keys) > 0:
-        client.delete_objects(
-            Bucket=s3_bucket,
-            Delete={'Objects': glacier_keys}
-        )
-# FIXME add this as debug        print("Deleted:\n\t %s" % "\n\t".join(map(lambda x: x['Key'], glacier_keys)))
+def main():
+    """Remove all matching objecs in storage class GLACIER from S3 bucket"""
+    session = get_session()
+    client = boto3.Session(botocore_session=session).client('s3')
 
-    if res['IsTruncated']:
-        next_token = res['NextContinuationToken']
-    else:
-        objects_left = False
-#    print('.', end='', flush=True)
-    sys.stdout.write("\r Iterated %s files, deleted %s files" % (iterated, deleted))
-    sys.stdout.flush()
+    objects_left = True
+    next_token = ''
+    iterated = 0
+    deleted = 0
+    while objects_left:
+        if next_token == '':
+            res = client.list_objects_v2(
+                Bucket=S3_BUCKET,
+                MaxKeys=1000,
+                Prefix=S3_PREFIX
+            )
+        else:
+            res = client.list_objects_v2(
+                Bucket=S3_BUCKET,
+                MaxKeys=1000,
+                Prefix=S3_PREFIX,
+                ContinuationToken=next_token
+            )
+
+        glacier_keys = [{'Key': obj['Key']}
+                        for obj in res['Contents']
+                        if obj['StorageClass'] == 'GLACIER']
+        iterated += len(res['Contents'])
+        deleted += len(glacier_keys)
+        if glacier_keys:
+            client.delete_objects(
+                Bucket=S3_BUCKET,
+                Delete={'Objects': glacier_keys}
+            )
+
+        if res['IsTruncated']:
+            next_token = res['NextContinuationToken']
+        else:
+            objects_left = False
+        sys.stdout.write("\r Iterated %s files, deleted %s files" % (iterated, deleted))
+        sys.stdout.flush()
+
+main()
