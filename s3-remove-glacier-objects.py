52db21637a12d2db343a57c76e32cae059a06812
Maik Glatki <maik@glatki.eu>
2018-06-07 08:46:07 +0000

    Add small AWS S3 glacier removal script with continuation tokens

---
+++ s3-remove-glacier-objects.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Removes s3 glacier objects from an s3 bucket
+# Author: Maik Glatki <maik@glatki.eu>, 2018
+
+from botocore import credentials
+import botocore.session
+import boto3
+import os
+import sys
+
+# Boto3 MFA session magic
+working_dir = os.path.join(os.path.expanduser('~'),'.aws/cli/cache')
+
+session = botocore.session.get_session()
+provider = session.get_component('credential_provider').get_provider('assume-role')
+provider.cache = credentials.JSONFileCache(working_dir)
+
+# Set Variables
+s3_bucket="example"
+s3_prefix="exampleprefix"
+
+client = boto3.Session(botocore_session=session).client('s3')
+
+objects_left = True
+next_token = ''
+while objects_left:
+    if next_token is '':
+        res = client.list_objects_v2(
+            Bucket=s3_bucket,
+            MaxKeys=1000,
+            Prefix=s3_prefix
+        )
+    else:
+        res = client.list_objects_v2(
+            Bucket=s3_bucket,
+            MaxKeys=1000,
+            Prefix=s3_prefix
+            ContinuationToken=next_token
+        )
+
+    glacier_keys = [{'Key': obj['Key']}  for obj in res['Contents'] if obj['StorageClass'] == 'GLACIER']
+    if len(glacier_keys) > 0:
+        client.delete_objects(
+            Bucket=s3_bucket,
+            Delete={'Objects': glacier_keys}
+        )
+        print("Deleted:\n\t %s" % "\n\t".join(map(lambda x: x['Key'], glacier_keys)))
+
+    if res['IsTruncated']:
+        next_token = res['NextContinuationToken']
+    else:
+        objects_left = False
